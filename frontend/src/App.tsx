5170e6a71087e442b1efccff194c6cf915c57344
larapollehn <lara.pollehn@googlemail.com>
2020-06-28 14:00:34 +0200

    backend edited

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<any, any>{
+  render(){
+    return (
+        <div className="App">
+          <header className="App-header">
+
+          </header>
+        </div>
+    );
+  }
+
+  componentDidMount() {
+        axios.get('https://tinylink.larapollehn.de/api/')
+            .then((response) => {
+            console.log('Backend responded', response.data);
+        }).catch((error) => {
+           console.log(error);
+        });
+  }
+
+
 }
 
 export default App;
