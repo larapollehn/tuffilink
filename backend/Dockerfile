c904fbc5835a51d5be33344550f596b0da93eb9d
larapollehn <lara.pollehn@googlemail.com>
2020-06-28 13:26:33 +0200

    setup

---
+++ backend/Dockerfile
@@ -0,0 +1,11 @@
+FROM node:alpine
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install --silent
+
+COPY . .
+
+CMD ["npm", "run", "start"]
---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send("Hello world GET");
+});
+
+router.post('/', function(req, res, next) {
+  res.send("Hello world POST");
+});
+
+module.exports = router;
