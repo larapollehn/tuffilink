aa5d1cf93f175129dbca59028a90f6240f1e6792
larapollehn <lara.pollehn@googlemail.com>
2020-07-03 14:42:58 +0200

    middleware

--- backend/src/routes/UrlRoutes.ts
+++ backend/src/routes/UrlRoutes.ts
@@ -3,15 +3,16 @@ import {
     createURL,
     getOriginalUrl,
     getUsersUrls,
-    deleteUrl} from "../services/UrlServices";
+    deleteUrl
+} from "../services/UrlServices";
+import {protectedPath} from "../middleware/AuthenticationMiddleWare";
 
 const urlRouter = express.Router();
 
-urlRouter.get('/', getUsersUrls);
-urlRouter.post('/', createURL);
+urlRouter.get('/', protectedPath, getUsersUrls);
+urlRouter.post('/', protectedPath, createURL);
 urlRouter.get('/original/:shorturl', getOriginalUrl);
-urlRouter.delete('/:url_id', deleteUrl);
-
+urlRouter.delete('/:url_id', protectedPath, deleteUrl);
 
 
 export default urlRouter;
