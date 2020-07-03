c4085b840f727141b993d5eecf5fd2cd9425aec2
larapollehn <lara.pollehn@googlemail.com>
2020-07-03 16:05:46 +0200

    sub

--- tests/tests/Base64.spec.ts
+++
@@ -1,9 +0,0 @@
-import Base64 from "../src/Base64";
-
-test('Base64 can encode and decode a string', () => {
-    const Base64Converter = new Base64();
-    let testString = 'https://dev.to/muhajirdev/unit-testing-with-typescript-and-jest-2gln';
-    let encodedTestString = Base64Converter.encodeBase64(testString);
-    let decodedTestString = Base64Converter.decodeBase64(encodedTestString);
-    expect(decodedTestString).toBe(testString);
-})
