8c9395572e0d74c21802f0fc51b1c9d25e04bfe7
larapollehn <lara.pollehn@googlemail.com>
2020-07-06 00:02:26 +0200

    modal

--- frontend/src/components/Userpage.tsx
+++ frontend/src/components/Userpage.tsx
@@ -15,6 +15,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import {Link} from "react-router-dom";
 import elephants from "./elephants_cropped.png";
 import Pagination from "react-bootstrap/Pagination";
+import {unwatchFile} from "fs";
+import Modal from "react-bootstrap/Modal";
 
 interface userPageProps {
 }
@@ -27,7 +29,9 @@ interface userPageState {
     pageSize: number;
     pageNumber: number;
     token: string;
-    pagination: any
+    pagination: any,
+    days: number;
+    show: boolean
 }
 
 class Userpage extends React.Component<userPageProps, userPageState> {
@@ -41,13 +45,18 @@ class Userpage extends React.Component<userPageProps, userPageState> {
             pageNumber: 0,
             pageSize: 5,
             token: '',
-            pagination: []
+            pagination: [],
+            days: 7,
+            show: false
         }
         this.getUrlCount = this.getUrlCount.bind(this);
         this.getUsersUrl = this.getUsersUrl.bind(this);
         this.processUrl = this.processUrl.bind(this);
         this.deleteToken = this.deleteToken.bind(this);
         this.changePage = this.changePage.bind(this);
+        this.showStatistics = this.showStatistics.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.buildStatistics = this.buildStatistics.bind(this);
     }
 
     async getUrlCount(userId: number, token: string) {
@@ -199,6 +208,39 @@ class Userpage extends React.Component<userPageProps, userPageState> {
         } else {
             log.debug('token not valid, next few urls could not be fetched');
         }
+    }
+
+    showStatistics(event: any){
+        let urlId = event.target.id;
+        let token = localStorageManager.getUserToken();
+        if(token && urlId){
+            axios({
+                method: 'GET',
+                url: `/api/click?link_id=${urlId}&days=${this.state.days}`,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((response) => {
+                log.debug('Statistics for clicked url', response.data);
+                this.setState({
+                    show: true
+                });
+                this.buildStatistics(response.data);
+            }).catch((error) => {
+                log.debug('Statistics could not be fetched', error.stack);
+            })
+        } else {
+            log.debug('token not valid, statistics could not be fetched');
+        }
+    }
+
+    handleClose(){
+        this.setState({
+            show: false
+        });
+    }
+
+    buildStatistics(data: []){
 
     }
 
@@ -218,6 +260,18 @@ class Userpage extends React.Component<userPageProps, userPageState> {
                         </Navbar.Collapse>
                     </Navbar>
                 </div>
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Statistics for the last {this.state.days} days</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
                 <img className={"elephantLogo"} src={elephants} alt="elephant-logo"/>
                 <div className="shortUserPageContainer">
@@ -246,6 +300,19 @@ class Userpage extends React.Component<userPageProps, userPageState> {
                         {
                             this.state.urls.map((url: { shorturl: string, originalurl: string, visit_count: number, id: number }, i: number) =>
                                 <ListGroup horizontal={"sm"} className="my-2" key={i}>
+                                    <ListGroup.Item className={"statisticGroupItem"}>
+                                        <button id={String(url['id'])} className={"deleteBtn"}
+                                                onClick={this.showStatistics}>
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16"
+                                             className="bi bi-bar-chart-line" fill="currentColor"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd"
+                                                  d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5h-2v12h2V2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"/>
+                                            <path fillRule="evenodd"
+                                                  d="M0 14.5a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
+                                        </svg>
+                                        </button>
+                                    </ListGroup.Item>
                                     <ListGroup.Item className={"firstGroupItem"}><a className={"tinylinkItem"}
                                                                                     href={`https://tinylink.larapollehn.de/${url['shorturl']}`}>https://tinylink.larapollehn.de/{url['shorturl']}</a></ListGroup.Item>
                                     <ListGroup.Item className={"originalUrlItem"}> {url['originalurl']}</ListGroup.Item>
